f0f027979c8bbece394095bf5015f71f4facade2
SecondBook5 <ajbook12@gmail.com>
2024-03-31 01:57:53 -0400

    add reverse complement

--- Algorithms/Replication.py
+++ Algorithms/Replication.py
@@ -1,15 +1,19 @@
 #Replication.py
 from collections import Counter
 
-
 def PatternCount(Text, Pattern):
     """
     Counts the occurrences of a given pattern in a text string.
+
     Args:
         Text (str): The text string to search for occurrences of the pattern.
         Pattern (str): The pattern string to search for in the text.
+
     Returns:
         int: The count of occurrences of the pattern in the text.
+        
+    Raises:
+        ValueError: If Text or Pattern is empty, or if Pattern length exceeds Text length.
     """
     # Check for empty inputs
     if not Text:
@@ -33,11 +37,16 @@ def PatternCount(Text, Pattern):
 def FrequencyMap(Text, k):
     """
     Generates a frequency map of substrings of length 'k' in the given text using a sliding window approach.
+
     Args:
         Text (str): The text to analyze.
         k (int): The length of substrings.
+
     Returns:
         dict: A dictionary where keys are substrings of length 'k' and values are their frequencies.
+        
+    Raises:
+        ValueError: If Text is empty or k is non-positive.
     """
     # Check for empty input or invalid value of k
     if not Text:
@@ -85,3 +94,42 @@ def FrequentWords(Text, k):
     frequent_words = [key for key, value in freq.items() if value == max_freq]
     # Return the list of most frequent substrings
     return frequent_words
+
+
+def ReverseComplement(Pattern):
+    """
+    Finds the reverse complement of a DNA sequence pattern.
+
+    Args:
+        Pattern (str): The DNA sequence pattern.
+
+    Returns:
+        str: The reverse complement of the input pattern.
+
+    Raises:
+        ValueError: If the input pattern contains invalid nucleotides or is empty.
+    """
+    # Set of valid nucleotides
+    valid_nucleotides = {'A', 'T', 'C', 'G', 'N'}
+
+    # Create a dictionary to store the complement of each nucleotide
+    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}
+
+    # Check if the input pattern is empty
+    if not Pattern:
+        raise ValueError("Input pattern cannot be empty")
+
+    # Initialize a list to store the reverse complement
+    reverse_complement = []
+
+    # Iterate through the input pattern in reverse order
+    for base in reversed(Pattern.upper()):
+        # Check if the current nucleotide is valid
+        if base not in valid_nucleotides:
+            raise ValueError("Invalid nucleotide in the input pattern")
+
+        # Find the complement of each nucleotide and append it to the reverse complement list
+        reverse_complement.append(complement_dict[base])
+
+    # Join the reverse complement list and return the result
+    return ''.join(reverse_complement)
